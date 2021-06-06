4dae8ad2e806af182c379b50c5f9f16a42376d57
Daniel Calvin <arockne2003@yahoo.com>
2021-06-06 06:59:18 -0700

    Added updateEmployeeWithKeyAndValue function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const employee = {
+  name: 'Daniel',
+  streetAddress: '1234 example lane, Denver CO, 80128'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, { [key]: value });
+}
